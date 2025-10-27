12849e57527315ac5965c43c4d5b1e892e7ab6ac
Omkar Patil <ompatil5132@gmail.com>
2025-10-27 19:13:15 +0530

    Added project files and README

---
+++ main.js
@@ -0,0 +1,52 @@
+const menuOpenButton = document.querySelector('#menu-open-button');
+
+menuOpenButton.addEventListener("click", () =>{
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+
+const menuCloseButton = document.querySelector('#menu-close-button');
+
+menuCloseButton.addEventListener("click", () =>
+    menuOpenButton.click()
+);
+
+// close menu when nav-link is clicked
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => menuOpenButton.click())
+})
+
+// initialize swiper 
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  spaceBetween:25,
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+//   responsive breakpoint
+  breakpoints :{
+    0:{
+        slidesPreView: 1
+    },
+    768:{
+        slidesPreView: 2
+    },
+    102:{
+        slidesPreView: 3
+    }
+  }
+});
